9483e4f2bf3ca7039f0b29913f1bc5411bd45bd4
kierepkae <kierepkaedyta@gmail.com>
2019-07-29 12:01:22 +0100

    add failing test for top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,14 @@
 require 'oystercard'
 describe Oystercard do
+
  it 'have initial balance' do
   expect(subject.balance).to eq(0)
  end
 
+ describe '#top_up' do
+
+  it {is_expected.to respond_to(:top_up).with(1).argument}
+
+ end
 
 end
