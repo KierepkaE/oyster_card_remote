6ed5cb21c94ed6d6bd61f575b8fb4ed67cd6e3ad
kierepkae <kierepkaedyta@gmail.com>
2019-07-29 12:51:58 +0100

    add failing test for deduct method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,7 +14,15 @@ describe Oystercard do
   end
 
   it 'throws an error if balance is over 90' do
-    expect{subject.top_up 99}.to raise_error 'Maximum balance exceeded'
+    max_balance = Oystercard::MAXIMUM_BALANCE
+    subject.top_up(max_balance)
+    expect{subject.top_up 1}.to raise_error "Maximum balance of #{max_balance } exceeded"
+  end
+ end
+
+ describe "#deduct" do
+  it 'charge 1 for a trip' do
+    expect{subject.deduct 1}.to change{subject.balance}.by 1
   end
  end
 
